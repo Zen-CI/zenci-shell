b083f1e944024a37c92b2836afdc3b45b5bf7d20
Gor Martsen <Gor.martsen@gmail.com>
2016-07-09 15:47:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,326 @@
 # zenci-shell
-Wrapper class for Node.js ssh2 shell command for running multiple commands
+Wrapper class for Node.js [ssh2](https://www.npmjs.org/package/ssh2) shell command for running multiple commands
 
 This class is based https://github.com/cmp-202/ssh2shell.
 Please check it first, maybe you do not need extra features provided by zenci-shell.
 
+*This class enables the following functionality:*
+* Run multiple commands sequentially within the context of the previous commands result.
+* Ability to respond to prompts resulting from a command as it is being run.
+* Ability to check the last command and conditions within the response text before the next command is run.
+* Performing actions based on command/response tests like adding or removing commands, processing of command response text.
+* Time tracking per each command.
+* Status from command.
+* Respond contain only command output.
+
+ 
+Installation:
+------------
+```
+npm install zenci-shell
+```
+
+Requirements:
+------------
+The class expects an object with the following structure to be passed to its constructor:
+```javascript
+//Host object
+host = {
+  server:              {       
+    host:         "IP Address",
+    port:         "external port number",
+    userName:     "user name",
+    password:     "user password",
+    passPhrase:   "privateKeyPassphrase", //optional string
+    privateKey:   require('fs').readFileSync('/path/to/private/key/id_rsa'), //optional string
+  },
+  commands:           ["Array", "of", "strings", "command"], //array() of command strings
+  msg:                {
+    send: function( message ) {
+      //message handler code
+	  console.log(message);
+    }
+  }, 
+  verbose:             false,  //optional boolean
+  debug:               false,  //optional boolean
+  idleTimeOut:         5000,        //optional number in milliseconds
+  connectedMessage:    "Connected", //optional string
+  readyMessage:        "Ready",     //optional string
+  closedMessage:       "Closed",    //optional string
+  
+  //optional event handlers defined for a host that will be called by the default event handlers
+  //of the class
+  onCommandProcessing: function( notice, sshObj, stream ) {
+   //optional code to run during the procesing of a command 
+   // notice is object with next properties
+   //   command: is the command being run
+   //   status: -1 on this phase
+   //   time: result of `new Date().getTime()` when command has been started.
+   //   output: current output for this command.
+   //sshObj is this object and gives access to the current set of commands
+   //stream object allows strea.write access if a command requires a response
+  },
+  onCommandComplete:   function( notice, sshObj ) {
+   //optional code to run on the completion of a command
+   // notice is object with next properties
+   //   command: is the command being run
+   //   status: exit code from processed command
+   //   time: time in miliseconds. This is how long last command was running for.
+   //   output: final output from command from start to end. Exclude command echo and shell prompt.
+   //sshObj is this object and gives access to the current set of commands
+  },
+  onCommandTimeout:    function(notice, sshObj, stream, connection) {
+   //optional code for responding to command timeout
+   // notice is object with next properties
+   //   command: is the command being run
+   //   status: 1 because we interrupted this command.
+   //   time: time in miliseconds. This is how long last command was running for.
+   //   output: final output from command from start to end. Exclude command echo and shell prompt.
+   //stream object used  to respond to the timeout without having to close the connection
+   //connection object gives access to close the shell using connection.end()
+  },
+  onEnd:               function( notices, sshObj ) {
+   //optional code to run at the end of the session
+   //noticess is array of all notices for each command.
+  }
+};
+```
+
+Minimal Example:
+
+```javascript
+var host = {
+  server:        {     
+    host:         "127.0.0.1",
+    userName:     "test",
+    password:     "1234",
+  },
+  commands:      [
+    "echo $(pwd)",
+    "ls -l"
+  ],
+  onEnd: function( notices, sshObj ) {
+    console.log( "onEnd" );
+    console.log( notices );
+  }
+};
+
+//Create a new instance
+var ZENCIShell = require ('zenci-shell'),
+    SSH       = new ZENCIShell(host);
+
+//Start the process
+SSH.connect();
+``` 
+
+
+Usage:
+======
+Connecting to a single host:
+----------------------------
+
+*How to:*
+* Use an .env file for server values loaded by dotenv from the root of the project.
+* Connect using a key pair with passphrase.
+* Set commands.
+* Test the response of a command and add more commands and notifications in the host.onCommandComplete event handler.
+* Use the two notification types in the commands array.
+
+
+*.env*
+```
+HOST=192.168.0.1
+PORT=22
+USER_NAME=myuser
+PASSWORD=mypassword
+PRIV_KEY_PATH=~/.ssh/id_rsa
+PASS_PHRASE=myPassPhrase
+```
+
+*app.js*
+```javascript
+var dotenv = require('dotenv');
+dotenv.load();
+var Email = require('email');
+
+var host = {
+ server:              {     
+  host:         process.env.HOST,
+  port:         process.env.PORT,
+  userName:     process.env.USER_NAME,
+  password:     process.env.PASSWORD,
+  passPhrase:   process.env.PASS_PHRASE,
+  privateKey:   require('fs').readFileSync(process.env.PRIV_KEY_PATH)
+ },
+ commands:      [
+  "echo $(pwd)",
+  "cd ~/",
+  "ls -l",
+  "echo $(pwd)",
+  "ls -l",
+ ],
+ onCommandComplete: function( notice, sshObj ) {
+  //confirm it is the root home dir and change to root's .ssh folder
+  if (notice.command === "echo $(pwd)" && response.indexOf("/root") != -1 ) {
+   sshObj.commands.push("cd .ssh");
+  }
+  //we are listing the dir so output it to the msg handler
+  else if (notice.command === "ls -l"){      
+   console.log(notice.output);
+  }
+ },
+ onEnd: function( notices, sshObj ) {
+   console.log(notices);
+ }
+};
+
+//Create a new instance
+var ZENCIShell = require ('zenci-shell'),
+    SSH       = new ZENCIShell(host);
+
+//Start the process
+SSH.connect();
+
+```
+
+Trouble shooting:
+-----------------
+
+* Recheck your passphrase for typos or missing chars.
+* Try connecting manually to the host using the exact passhrase used by the code to confirm it works.
+* If your password is incorrect the connection will return an error.
+* There is an optional debug setting in the host object that will output progress information when set to true and passwords for failed authentication of sudo commands and tunnelling. `host.debug = true`
+* The class now has an idle time out timer (default:5000ms) to stop unexpected command prompts from causing the process hang without error. The default time out can be changed by setting the host.idleTimeOut with a value in milliseconds. (1000 = 1 sec)
+
+Authentication:
+---------------
+* When using key authentication you may require a valid passphrase if your key was created with one. 
+
+
+Verbose and Debug:
+------------------
+* When verbose is set to true each command response raises a msg event (calls host.msg.send(message)) when the command completes.
+* When debug is set to true in a host object process messages raises a msg event (calls host.msg.send(message)) to help identify what the internal process of each step was. 
+
+Responding to command prompts:
+----------------------
+When running commands there are cases that you might need to respond to specific prompt that results from the command being run.
+The command response check method is the same as in the example for the host.onCommandComplete event handler but in this case we use it in the host.onCommandProcessing event handler and stream.write to send the response. If you want to terminate the connection then se the 
+The stream object is available in the host.onCommandProcessing event handler to output the response to the prompt directly as follows:
+
+```javascript
+//in the host object definition that will be used only for that host
+host.onCommandProcessing = function( notice, sshObj, stream ) {
+   //Check the command and prompt exits and respond with a 'y'
+   if (notice.command == "apt-get install nano" && notice.output.indexOf("[y/N]?") != -1 ) {
+     stream.write('y\n');
+   }
+ };
+ 
+ // Alternativly you can use this one instead of defining in host.
+ //This will be run in addition to any other handlers defined for this event
+ zenci-shell.on ('commandProcessing', function onCommandProcessing( notice, sshObj, stream ) {
+   //Check the command and prompt exits and respond with a 'y'
+   if (notice.command == "apt-get install nano" && notice.output.indexOf("[y/N]?") != -1 ) {
+     stream.write('y\n');
+   }
+ };
+
+```
+The other alternative is to use the host.onCommandTimeout event handler but it will be delayed by the idleTimout value
+
+```javascript
+host.onCommandTimeout = function( notice, sshObj, stream, connection ) {
+   if (notice.output.indexOf("[y/N]?") != -1 ) {
+     stream.write('n\n');
+   }
+ }
+```
+To terminate the session on such a prompt use connection.end() within the timeout event handler.
+
+
+Event Handlers:
+---------------
+There are a number of event handlers that enable you to add your own code to be run when those events are triggered. You do not have to add event handlers unless you want to add your own functionality as the class already has default handlers defined. 
+
+There are two ways to add event handlers:
+
+1. Add handller functions to the host object (See Requirments) these event handlers will only be run for that host.
+ * Connect, ready and close are not available for definition in the hosts object 
+2. Add handlers to the class instance which will be run every time the event is triggered for all hosts.
+ * The default event handlers of the class will call the host object event handler functions if they are defined.
+
+**Note:** any event handlers you add to the class instance are run as well as any other event handlers defined for the same event.
+
+*Further reading:* [node.js event emitter](http://nodejs.org/api/events.html#events_class_events_eventemitter)
+
+**Class Instance Event Definitions:**
+
+```javascript
+zenci-shell.on ("connect", function onConnect() { 
+ //default: outputs primaryHost.connectedMessage
+});
+
+zenci-shell.on ("ready", function onReady() { 
+ //default: outputs primaryHost.readyMessage
+});
+
+zenci-shell.on ("msg", function onMsg( message ) {
+ //default: outputs the message to the host.msg.send function. If undefined output mutted.
+ //message is the text to ouput.
+});
+
+zenci-shell.on ("commandProcessing", function onCommandProcessing( notice, sshObj, stream )  { 
+ //default: runs host.onCommandProcessing function if defined 
+   // notice is object with next properties
+   //   command: is the command being run
+   //   status: -1 on this phase
+   //   time: result of `new Date().getTime()` when command has been started.
+   //   output: final output from command from start to end. Exclude command echo and shell prompt.
+ //sshObj is the host object
+ //stream is the session stream
+});
+    
+zenci-shell.on ("commandComplete", function onCommandComplete( notice, sshObj ) { 
+ //default: runs host.onCommandComplete function if defined 
+   // notice is object with next properties
+   //   command: is the command being run
+   //   status: command exit code.
+   //   time: time in miliseconds. This is how long last command was running for.
+   //   output: final output from command from start to end. Exclude command echo and shell prompt.
+ //sshObj is the host object
+});
+    
+zenci-shell.on ("commandTimeout", function onCommandTimeout( notice, stream, connection ) { 
+ //default: runs host.onCommandTimeout function if defined if not the buffer is added to sessionText
+ //the error is outputed to the msg event and the connection is closed
+ // notice is object with next properties
+ //   command: is the command being run
+ //   status: 1 because we interrupted this command.
+ //   time: time in miliseconds. This is how long last command was running for.
+ //   output: final output from command from start to end. Exclude command echo and shell prompt.
+ //stream is the session stream
+ //connection is the main ssh2 connection object
+});
+
+zenci-shell.on ("end", function onEnd( notices, sshObj ) { 
+ //default: run host.onEnd function if defined 
+ //sessionText is the full text response from the session for the host
+ //sshObj is the host object for this session
+});
+
+zenci-shell.on ("close", function onClose(had_error) { 
+ //default: outputs primaryHost.closeMessage or error if one was received
+ //had_error indicates an error was recieved on close
+});
+
+zenci-shell.on ("error", function onError(err, type, close, callback) {
+ //default: Outputs the error, runs the callback if defined and closes the connection
+ //Close and callback should be set by default to close = false and callback = undefined
+ //not all error events will pass those two parameters to the evnt handler.
+ //err is the error message received
+ //type is a string identifying the source of the error
+ //close is a bollean value indicating if the error will close the session
+ //callback a fuction that will be run by the default handler
+});
+```
---
+++ zenci-shell.js
@@ -0,0 +1,383 @@
+/**
+ * Originaly based on ssh2shell package but adjusted for Zen.CI project.
+ * credit: https://github.com/cmp-202/ssh2shell
+ * Author: http://github.com/Gormartsen
+ */
+"use strict";
+
+var EventEmitter = require("events").EventEmitter;
+var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; }
+var hasProp = {}.hasOwnProperty;
+
+var ZENCIShell = (function(superClass) {
+    extend(ZENCIShell, superClass);
+
+    ZENCIShell.prototype.sshObj = {};
+
+    ZENCIShell.prototype.command = "";
+
+    ZENCIShell.prototype._origin_command = "";
+
+    ZENCIShell.prototype._origin_command_output = "";
+
+    ZENCIShell.prototype._notices = [];
+
+    ZENCIShell.prototype._status = -1;
+
+    ZENCIShell.prototype._start_time = 0;
+
+    ZENCIShell.prototype._total_time = 0;
+
+    ZENCIShell.prototype._stream = {};
+
+    ZENCIShell.prototype._data = "";
+
+    ZENCIShell.prototype._buffer = "";
+
+    /**
+     * Timeout event handler.
+     *   Emit 'commandTimeout' event if timeout happend.
+     *    - _notice object with last command details.
+     *    - _stream ssh2-stream object
+     *    - connection ssh2 object
+     */
+    ZENCIShell.prototype._timedout = function() {
+      var _notice = {
+        'command': this.command,
+        'status': 1, //Set status to 1 as failed to timed out command.
+        'time': new Date().getTime() - this._start_time,
+        'output': this._buffer
+      }
+      this._notices[this.command] = _notice;
+      return this.emit('commandTimeout', _notice, this._stream, this.connection);
+    };
+
+    /**
+     * Process output from stream.
+     *   Emit 'commandProcessing' event if command did not finish yet.
+     *    - _notice object with last command details.
+     *    - sshObj - settings for this class.
+     *    - _stream ssh2-stream object
+     */
+    ZENCIShell.prototype._processData = function(data) {
+      this._buffer += data;
+      if (this.standardPromt.test(this._buffer)) {
+        if (this.sshObj.debug) {
+          this.emit('msg', this.sshObj.server.host + ": normal prompt");
+        }
+        // -13 is a length of hardcoded PS1 + COMMAND_PROMPT.
+        if(this._buffer.length >= 13){
+          this._buffer = this._buffer.substr(0, this._buffer.length - 13);
+        }
+        //cut out cmd line from buffer.
+        if(this.command.length +2 <= this._buffer.length) {
+          this._buffer =  this._buffer.substr(this.command.length + 2);
+        }
+        if(this.command == 'echo -e "$?"' ) {
+          this._status = parseInt(this._buffer);
+        }
+        return this._processNextCommand();
+      } else {
+        var _notice = {
+          'command': this.command,
+          'status': this._status,
+          'time': new Date().getTime() - this._start_time,
+          'output': this._buffer
+        }
+        this.emit('commandProcessing', _notice, this.sshObj, this._stream);
+        if (this._idleTimer) {
+          clearTimeout(this._idleTimer);
+        }
+        return this._idleTimer = setTimeout(this._timedout, this._idleTime);
+      }
+    };
+    /**
+     * Prepare to start next Command.
+     *   Emit 'commandComplete' event if command finished,
+     *    - _notice object with last command details.
+     *    - sshObj - settings for this class.
+     *   or start 'echo -e "$?"' to get latest comman status.
+     */
+    ZENCIShell.prototype._processNextCommand = function() {
+      if (this._status === -1 && this.command !== '' && this.command !== 'echo -e "$?"' ) {
+        this._origin_command = this.command;
+        this._origin_command_output = this._buffer;
+        this._total_time = new Date().getTime() - this._start_time;
+        this.command = 'echo -e "$?"';
+        this._buffer = "";
+        return this._runCommand();
+      }
+      if(this.command === 'echo -e "$?"' ) {
+        this.command = this._origin_command;
+      }
+      // we are receiving banner only when command is empty.
+      if(this.command !== "") {
+        var _notice = {
+          'command': this.command,
+          'status': this._status,
+          'time': this._total_time,
+          'output': this._origin_command_output
+        }
+        this._notices[this.command] = _notice;
+        this.emit('commandComplete', _notice, this.sshObj);
+      }
+      if (this.sshObj.verbose) {
+        this.emit('msg', this.sshObj.server.host + " verbose:" + this._buffer);
+      }
+      this._buffer = "";
+      if (this.sshObj.commands.length > 0) {
+        this.command = this.sshObj.commands.shift();
+        this._status = -1;
+        this._start_time = new Date().getTime();
+        if (this.command) {
+          return this._runCommand();
+        } else {
+          return this._runExit();
+        }
+      } else {
+        return this._runExit();
+      }
+    };
+    /**
+     * Write command to stream.
+     */
+    ZENCIShell.prototype._runCommand = function() {
+      if (this.sshObj.debug) {
+        this.emit('msg', this.sshObj.server.host + ": next command: " + this.command);
+      }
+      return this._stream.write(this.command + "\n");
+    };
+
+    ZENCIShell.prototype._runExit = function() {
+      this.command = "exit";
+      if (this.sshObj.exitCommands && this.sshObj.exitCommands.length > 0) {
+        if (this.sshObj.debug) {
+          this.emit('msg', this.sshObj.server.host + ": Queued exit commands: " + this.sshObj.exitCommands);
+        }
+        this.command = this.sshObj.exitCommands.pop();
+        return this._runCommand();
+      } else {
+        if (this.sshObj.debug) {
+          this.emit('msg', "Exit and close connection on: " + this.sshObj.server.host);
+        }
+        return this._stream.end("exit\n");
+      }
+    };
+    /**
+     * Set default values for settings.
+     */
+    ZENCIShell.prototype._loadDefaults = function() {
+      var ref;
+      if (!this.sshObj.msg) {
+        this.sshObj.msg = {
+          send: (function(_this) {
+            return function(message) {
+              return false;
+            };
+          })(this)
+        };
+      }
+      if (!this.sshObj.connectedMessage) {
+        this.sshObj.connectedMessage = "Connected";
+      }
+      if (!this.sshObj.readyMessage) {
+        this.sshObj.readyMessage = "Ready";
+      }
+      if (!this.sshObj.closedMessage) {
+        this.sshObj.closedMessage = "Closed";
+      }
+      if (!this.sshObj.verbose) {
+        this.sshObj.verbose = false;
+      }
+      if (!this.sshObj.debug) {
+        this.sshObj.debug = false;
+      }
+      if (!this.sshObj.passphrasePromt) {
+        this.sshObj.passphrasePromt = ":";
+      }
+      this.sshObj.exitCommands = [];
+      this.sshObj.pwSent = false;
+      this.sshObj.sshAuth = false;
+      this._idleTime = (ref = this.sshObj.idleTimeOut) != null ? ref : 5000;
+      this.passphrasePromt = new RegExp("password.*" + this.sshObj.passphrasePromt + "\\s?$", "i");
+      return this.standardPromt = new RegExp("zencishell: $");
+    };
+    /**
+     * Object constructor.
+     */
+    function ZENCIShell(sshObj1) {
+      this.sshObj = sshObj1;
+      this.connect = bind(this.connect, this);
+      this._loadDefaults = bind(this._loadDefaults, this);
+      this._runExit = bind(this._runExit, this);
+      this._runCommand = bind(this._runCommand, this);
+      this._processNextCommand = bind(this._processNextCommand, this);
+      this._processData = bind(this._processData, this);
+      this._timedout = bind(this._timedout, this);
+      this._loadDefaults();
+      this.connection = new require('ssh2')();
+      this.on("connect", (function(_this) {
+        return function() {
+          return _this.emit('msg', _this.sshObj.connectedMessage);
+        };
+      })(this));
+      this.on("ready", (function(_this) {
+        return function() {
+          return _this.emit('msg', _this.sshObj.readyMessage);
+        };
+      })(this));
+      this.on("msg", (function(_this) {
+        return function(message) {
+          if (_this.sshObj.msg) {
+            return _this.sshObj.msg.send(message);
+          }
+        };
+      })(this));
+      this.on('commandProcessing', (function(_this) {
+        return function(notice, sshObj, stream) {
+          if (_this.sshObj.onCommandProcessing) {
+            return _this.sshObj.onCommandProcessing(notice, sshObj, stream);
+          }
+        };
+      })(this));
+      this.on('commandComplete', (function(_this) {
+        return function(notice, sshObj) {
+          if (_this.sshObj.onCommandComplete) {
+            return _this.sshObj.onCommandComplete(notice, sshObj);
+          }
+        };
+      })(this));
+      this.on('commandTimeout', (function(_this) {
+        return function(notice, stream, connection) {
+          if (_this.sshObj.onCommandTimeout) {
+            return _this.sshObj.onCommandTimeout(notice, stream, connection);
+          } else {
+            return _this.emit("error", _this.sshObj.server.host + ": Command timed out after " + (_this._idleTime / 1000) + " seconds", "Timeout", true, function(err, type) {
+              return _this._buffer;
+            });
+          }
+        };
+      })(this));
+      this.on('end', (function(_this) {
+        return function(notices, sshObj) {
+          if (_this.sshObj.onEnd) {
+            return _this.sshObj.onEnd(notices, sshObj);
+          }
+        };
+      })(this));
+      this.on("close", (function(_this) {
+        return function(had_error) {
+          if (had_error) {
+            return _this.emit("error", had_error, "Close");
+          } else {
+            return _this.emit('msg', _this.sshObj.closedMessage);
+          }
+        };
+      })(this));
+      this.on("error", (function(_this) {
+        return function(err, type, close, callback) {
+          if (close == null) {
+            close = false;
+          }
+          _this.emit('msg', (type + " error: ") + err);
+          if (callback) {
+            callback(err, type);
+          }
+          if (close) {
+            return _this.connection.end();
+          }
+        };
+      })(this));
+    }
+    /**
+     * Init ssh2 connection.
+     * by using export we setup our own prompt to detect when command finished.
+     *   Emit 'onEnd' event if command finished,
+     *    - _notices array of notice objects with command status.
+     *    - sshObj - settings for this class.
+     */
+    ZENCIShell.prototype.connect = function() {
+      var e, error, ref, ref1;
+      if (this.sshObj.server && this.sshObj.commands) {
+        try {
+          this.connection.on("connect", (function(_this) {
+            return function() {
+              return _this.emit("connect");
+            };
+          })(this));
+          this.connection.on("ready", (function(_this) {
+            return function() {
+              _this.emit("ready");
+              return _this.connection.shell({'cols': 1000, 'modes': {'ECHO': 53}},function(err, _stream) {
+                _this._stream = _stream;
+                if (err) {
+                  _this.emit('error', err, "Shell", true);
+                }
+                _this._stream.write("export PS1='zencishell: ';export PROMPT_COMMAND='echo -n \"_\"' ; sleep 1\n");
+                _this._stream.on("error", function(err) {
+                  return _this.emit('error', err, "Stream");
+                });
+                _this._stream.stderr.on('data', function(data) {
+                  return _this.emit('stderr', data, "Stream STDERR");
+                });
+                _this._stream.on("readable", function() {
+                  var data, e, error, results;
+                  try {
+                    results = [];
+                    while ((data = _this._stream.read())) {
+                      results.push(_this._processData("" + data));
+                    }
+                    return results;
+                  } catch (error) {
+                    e = error;
+                    return _this.emit('error', e + " " + e.stack, "Processing response:", true);
+                  }
+                });
+                _this._stream.on("end", function() {
+                  return _this.emit('end', _this._notices, _this.sshObj);
+                });
+                return _this._stream.on("close", function(code, signal) {
+                  if (_this._idleTimer) {
+                    clearTimeout(_this._idleTimer);
+                  }
+                  return _this.connection.end();
+                });
+              });
+            };
+          })(this));
+          this.connection.on("error", (function(_this) {
+            return function(err) {
+              return _this.emit("error", err, "Connection", true);
+            };
+          })(this));
+          this.connection.on("close", (function(_this) {
+            return function(had_error) {
+              return _this.emit("close", had_error);
+            };
+          })(this));
+          return this.connection.connect({
+            host: this.sshObj.server.host,
+            port: this.sshObj.server.port,
+            username: this.sshObj.server.userName,
+            password: this.sshObj.server.password,
+            privateKey: (ref = this.sshObj.server.privateKey) != null ? ref : "",
+            passphrase: (ref1 = this.sshObj.server.passPhrase) != null ? ref1 : ""
+          });
+        } catch (error) {
+          e = error;
+          return this.emit('error', e + " " + e.stack, "Connect:", true);
+        }
+      } else {
+        return this.emit('error', "Missing connection parameters", "Parameters", false, missingParameters(err, type, close)(function() {
+          this.emit('msg', this.sshObj.server);
+          return this.emit('msg', this.sshObj.commands);
+        }));
+      }
+    };
+
+    return ZENCIShell;
+
+  })(EventEmitter);
+
+module.exports = ZENCIShell;
